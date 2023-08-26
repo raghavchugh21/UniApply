718f04ec4663342b6051797e7d074c08065df0d7
Raghav Chugh <raghavchughcode@gmail.com>
2023-08-27 02:40:58 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-pip install -r requirements.txt
-python --version
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic
+python3.9 -m pip install -r requirements.txt
+python3.9 --version
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic
