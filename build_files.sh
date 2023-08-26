62f8182be0586b7400cf54d19e3c43d7a787d41f
Raghav Chugh <raghavchughcode@gmail.com>
2023-08-27 03:01:02 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 python3.9 -m pip install -r requirements.txt
-python3.9 --version
+sudo apt install libsqlite3-dev
+./configure --enable-loadable-sqlite-extensions && make && sudo make install
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic
