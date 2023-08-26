05a2005426ca1c6c04d861ae7fa1ee87a2c7dac6
Raghav Chugh <raghavchughcode@gmail.com>
2023-08-27 03:49:37 +0530

    update

--- UniApply/settings.py
+++ UniApply/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'user_visit.middleware.UserVisitMiddleware',
 ]
 
