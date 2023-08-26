c3fe82cae09dfdf54a606f9c8ada18921b5d3931
Raghav Chugh <raghavchughcode@gmail.com>
2023-08-27 01:49:22 +0530

    add

--- UniApply/urls.py
+++ UniApply/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
  
 urlpatterns = [
